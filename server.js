885061aefc47824c88a02e1788eccc9d481c6217
elmfer <elmfer11@outlook.com>
2023-11-21 23:21:10 -0600

    notify when connection to database was successful

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use((req, res) => {
 
 // Initialize connection to database and server
 dbConnection.once('open', () => {
+  console.log('Connected to database');
+
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
